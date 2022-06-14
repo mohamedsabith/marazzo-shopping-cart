82f794b3e32fd8208e49934568ee1a1c7a50a471
mohamedsabith <sabithmohd101@gmail.com>
2022-06-14 18:05:12 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -2,14 +2,24 @@ const mongoose = require('mongoose');
 var colors = require('colors');
 require('dotenv').config()
 
-mongoose.connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-})
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
-mongoose.connection.on("error", err => {
-  console.log("err".red, err)
-})
+// mongoose.connect(process.env.CONNECTION_URL, {
+//     useNewUrlParser: true,
+// })
 
-mongoose.connection.on("connected", (err, res) => {
-    console.log('mongoose is connected');
-})
+// mongoose.connection.on("error", err => {
+//   console.log("err".red, err)
+// })
+
+// mongoose.connection.on("connected", (err, res) => {
+//     console.log('mongoose is connected');
+// })
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((_) => {
+   console.log("mongoose is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
